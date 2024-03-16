5f22d750aa511e171a042337bf5f69faa8459f25
CopeOnline <Cop21004@byui.edu>
2024-03-16 15:03:50 -0600

    rolled get request back to working code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -8,15 +8,12 @@ const getAll = async (req, res) => {
   /*
     #swagger.description = 'Returns all contacts in the database.';
     */
-  const result = await mongodb.getDatabase().db().collection('contacts').find()
-  result.toArray(err).then((contacts) => {
-    if (err) {
-      res.status(400).json({ message: err })
-    }
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).json(contacts)
-  })
-}
+  const result = await mongodb.getDatabase().db().collection('contacts').find();
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', "application/json");
+    res.status(200).json(contacts);
+  });
+};
 
 const getSingle = async (req, res) => {
   // swagger.tags=['contacts']
@@ -27,21 +24,18 @@ const getSingle = async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     res.status(400).json('Must use a valid contact id to find a contact.')
   }
-  const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId })
-  result.toArray(err).then((contacts) => {
-    if (err) {
-      res.status(400).json({ message: err })
-    }
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).json(contacts[0])
-  })
-}
+  const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId });
+  result.toArray().then((contacts) => {
+   res.setHeader('Content-Type', "application/json");
+   res.status(200).json(contacts[0]);
+  });
+};
 
 const createContact = async (req, res) => {
   // swagger.tags=['contacts']
   /*
     #swagger.description = 'Create a contact in the database, every field is required. The contacted ID number is automatically assigned by the database after submition.
-                            Any field that is ommitted will be set to "NULL"';
+    Any field that is ommitted will be set to "NULL"';
     */
   const contact = {
     firstName: req.body.firstName,
@@ -62,7 +56,7 @@ const updateContact = async (req, res) => {
   // swagger.tags=['contacts']
   /*
     #swagger.description = 'Update a contacts information using the contacts ID number.The contacted ID number is automatically assigned by the database after submition.
-                            Any field that is ommitted will be set to "NULL"';
+     Any field that is ommitted will be set to "NULL"';
     */
   if (!ObjectId.isValid(req.params.id)) {
     res.status(400).json('Must use a valid contact id to update a contact.')
